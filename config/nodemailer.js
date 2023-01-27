1cd95d9c1ce06e349e6fff5d520984c894beeba7
UmarFaiq <umarfaiq@pop-os.localdomain>
2023-01-28 02:58:37 +0530

    make config changes for security

---
+++ config/nodemailer.js
@@ -0,0 +1,44 @@
+//be sure to add this file to .gitignore in order to not put your email and password on github
+const nodemailer = require("nodemailer");
+const ejs = require('ejs');
+const path = require('path');
+
+
+let testAccount = (async function () {
+    return await nodemailer.createTestAccount();
+})();
+
+// let testAccount = nodemailer.createTestAccount();
+
+//transport object is needed to define the config of how to send the email
+let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+        user: '', //your email
+        pass: '' // your password
+    }
+});
+
+let renderTemplate = (data, relativePath) => {
+    let mailHTML;
+
+    //read the ejs file that has the email template defined and render the mail template html file given in the path
+    ejs.renderFile(
+        path.join(__dirname, '../views/mailers', relativePath),
+        data,
+        function (err, template) {
+            if (err) { console.log('error in rendering the template at path', path.join(__dirname, '../views/mailers', relativePath)); return; }
+
+            mailHTML = template;
+        }
+    );
+
+    return mailHTML;
+}
+
+module.exports = {
+    transporter: transporter,
+    renderTemplate: renderTemplate
+}
